a7e2270e78242213d7c296412c461f13be846300
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2025-11-24 19:32:10 +0530

    Feedback 2 done

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
 
   return (
     <div className="py-24 px-8 md:px-16 lg:px-24 flex flex-col justify-center items-center gap-16" ref={ref}>
-      <div className='flex flex-col lg:flex-row items-center gap-8 lg:gap-16'>
+      <div className='flex flex-col items-center gap-8 lg:gap-16'>
         <div className="flex flex-col items-center gap-4">
           <motion.img src={Logo} alt="DailySpend Logo" className="w-28 lg:w-32" 
             initial = {{opacity: 0, scale: 0.5}}
@@ -56,7 +56,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-2 gap-16 text-center">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-16 text-center">
           <div>
             <h3 className="text-4xl font-bold text-black">{inView && <CountUp end={95} duration={4} />}K+</h3>
             <p className="text-gray-600 mt-2">Downloads</p>
